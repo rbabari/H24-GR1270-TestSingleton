bc87c545725769b363957013ef94e03adb7de07d
rbabari <r.babari@gmail.com>
2024-06-27 14:26:26 -0700

    ajout java doc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Singleton","l":"faireQqchose(int, String)","u":"faireQqchose(int,java.lang.String)"},{"p":"<Unnamed>","c":"Singleton","l":"getInstance()"},{"p":"<Unnamed>","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- src/Singleton.java
+++ src/Singleton.java
@@ -1,19 +1,42 @@
+/**
+ * Classe exemple singleton *********YEEEEEEEEEEEEEEEEEEEEE ...
+ // * @author rb
+ // *
+ */
 public class Singleton {
     private int nbrExecution =0;
     // - instance
     private static Singleton instance = null;
 
     // - Singelton()
+ /**
+ lasuikfh
+ dfvkjbdfvv
+ dfsvkjdnfv
+ dfsvlkndfv
+  */
+
+
+
+
+    /**
+     * contructeur privé du singleton ...
+     */
     private Singleton()
     {
         System.out.println("constructeur singleton");
         nbrExecution++;
-
+// mon propre commentaire ,,, non inclut dans la java doc ....
     }
 
 
 
     // + getInstance()
+
+    /**
+     *  methode qui retourne l'instance unique du sigleton
+     * @return singleton
+     */
     public static Singleton getInstance()
     {
         if(instance == null)
@@ -23,4 +46,17 @@ public class Singleton {
         System.out.println("getInstance() ");
         return instance;
     }
+
+    /**
+     * methode qui fait qqchose
+     * @param p1 est un int qui represente qqchose
+     * @param nom est un string ...
+     * @return retourne zero ou la sommme
+     */
+    public static int faireQqchose(int p1, String nom)
+    {
+
+        return 0;
+    }
+
 }
